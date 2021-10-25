d8fa70e29877b4347bcc0b7fe15f6149cf173097
u18335412@tuks.co.za <u18335412@tuks.co.za>
2021-10-25 14:36:55 +0200

    removed docs directory

--- docs/_next/static/TCG2EvkngwvhF_Ea9tBO5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-d394981b3a55f6a02e74.js","static/chunks/pages/index-714dbecf4fdb8b25963c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
