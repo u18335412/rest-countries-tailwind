388dcf03ac9900f78841fbd2730add9480125454
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-02-27 17:53:09 +0200

    refactor for framer-motion

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import SearchSection from "../src/components/SearchBar";
 import { getAllData } from "../src/data/data";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
 
 export const getStaticProps = async (context) => {
   const data = await getAllData();
@@ -12,10 +13,62 @@ export const getStaticProps = async (context) => {
   };
 };
 
+function Country({ c }) {
+  return (
+    <Link href={`/${c.name.common}`}>
+      <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0.5 }}
+        layout
+        className="flex flex-col pb-10 mt-5 mb-5 transition-shadow rounded-t-lg shadow-lg cursor-pointer dark:bg-grayishDarkBlue hover:shadow-2xl h-5/6 w-12/12 md:w-3/3 lg:w-4/4"
+      >
+        <div>
+          <div>
+            <img
+              src={c.flags.svg}
+              className="object-cover rounded-t-lg w-80 h-44"
+              alt={`${c.name.common} flag`}
+            ></img>
+          </div>
+          <div className="h-full pt-5 pl-5">
+            <label className="font-bold">{c.name.common}</label>
+            <div className="mt-2">
+              <span className="mr-2 text-sm font-semibold ">Population</span>
+              <span className="text-sm"> {c.population}</span>
+            </div>
+            <div className="mt-2">
+              <span className="mr-2 text-sm font-semibold ">Region</span>
+              <span className="text-sm">{c.region}</span>
+            </div>
+            <div className="mt-2">
+              <span className="mr-2 text-sm font-semibold ">Capital</span>
+              <span className="text-sm">{c.capital?.toString()}</span>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </Link>
+  );
+}
+
 const Home = ({ countries }) => {
-  const router = useRouter();
-  const [region, setRegion] = useState("none");
+  // const router = useRouter();
+  const [region, setRegion] = useState("None");
   const [searchValue, setSearchValue] = useState("");
+  const [filterd, setFilterd] = useState(countries);
+
+  useEffect(() => {
+    setFilterd(
+      countries.filter(
+        (c) =>
+          c.name.common
+            .toLowerCase()
+            .includes(searchValue.toLocaleLowerCase()) === true &&
+          (region === "None" || (region !== "None" && c.region === region))
+      )
+    );
+  }, [region, searchValue]);
 
   const toggle = (value) => {
     setRegion(value);
@@ -25,85 +78,33 @@ const Home = ({ countries }) => {
     setSearchValue(value);
   };
 
-  const CountryComponent = ({ countries }) => {
-    return (
-      <>
-        <Head>
-          <title>Countries</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <section className="mt-5 gap-x-20 grid place-items-center md:grid-cols-3 lg:grid-cols-4 ">
-          {countries.map((c, idx) => {
-            if (c.name.common.toLowerCase().includes(searchValue.toLocaleLowerCase()) === false) return;
-            const capitals = c.capital;
-            if (
-              region === "none" ||
-              (region !== "none" && c.region === region)
-            ) {
-              return (
-                <Link href={`/${c.name.common}`}>
-                  <div
-                    key={idx}
-                    className="rounded-t-lg dark:bg-grayishDarkBlue cursor-pointer hover:shadow-xl  h-5/6 mb-5 flex flex-col shadow-lg pb-10 mt-5 w-12/12 md:w-3/3 lg:w-4/4"
-
-                  >
-                    <>
-                      <div>
-
-                        <img
-                          src={c.flags.svg}
-                          className=" rounded-t-lg object-cover w-80 h-44"
-                          alt={`${c.name.common} flag`}
-                        ></img>
-
-                      </div>
-                      <div className="pt-5 pl-5  h-full">
-                        <label className="font-bold">{c.name.common}</label>
-                        <div className="mt-2">
-                          <span className=" font-semibold text-sm mr-2">
-                            Population
-                          </span>
-                          <span className="text-sm"> {c.population}</span>
-                        </div>
-                        <div className="mt-2">
-                          <span className=" font-semibold text-sm mr-2">
-                            Region
-                          </span>
-                          <span className="text-sm">{c.region}</span>
-                        </div>
-                        <div className="mt-2">
-                          <span className=" font-semibold text-sm mr-2">
-                            Capital
-                          </span>
-                          <span className="text-sm">
-                            {capitals === undefined ? "" : capitals.toString()}
-                          </span>
-                        </div>
-                      </div>
-                    </>
-                  </div>
-                </Link>
-              );
-            }
-          })}
-        </section>
-      </>
-    );
-  };
   return (
     <>
-      <div className="px-5 2xl:px-28 dark:bg-veryDarkBlue dark:text-white min-h-screen">
+      <Head>
+        <title>Countries</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="min-h-screen px-5 2xl:px-28 dark:bg-veryDarkBlue dark:text-white">
         <SearchSection
           toggle={toggle}
           input={searchValue}
           update={handleChange}
         ></SearchSection>
-        <CountryComponent countries={countries}></CountryComponent>
+        <motion.div
+          layout
+          className="grid mt-5 gap-x-20 place-items-center md:grid-cols-3 lg:grid-cols-4 "
+        >
+          <AnimatePresence>
+            {filterd.map((c) => {
+              return <Country key={c.name.common} c={c}></Country>;
+            })}
+          </AnimatePresence>
+        </motion.div>
       </div>
     </>
   );
