276875482b78dc041212d0e8cfb61006e5dc72ae
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-02-27 17:54:38 +0200

    Animate icon presence

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,48 @@
 import { HiOutlineMoon } from "react-icons/hi";
 import { MdOutlineLightMode } from "react-icons/md";
+import { motion, AnimatePresence } from "framer-motion";
+
 const Navbar = (props) => {
   return (
     <>
-      <section className=" dark:bg-grayishDarkBlue dark:text-white px-5 2xl:px-28  shadow-md flex text-xs md:text-base justify-between items-center justify-items-center  py-3">
+      <section className="flex items-center justify-between px-5 py-3 text-xs shadow-md dark:bg-grayishDarkBlue dark:text-white 2xl:px-28 md:text-base justify-items-center">
         <div>
-          <label className=" font-bold text-xs font-bold md:text-xl">
+          <label className="text-xs font-bold md:text-xl">
             Where in the world?
           </label>
         </div>
         <div>
           <div
             onClick={props.toggleTheme}
-            className="py-3 rounded-md flex justify-center items-center hover:cursor-pointer"
+            className="flex items-center justify-center py-3 rounded-md hover:cursor-pointer"
           >
-            {props.theme === "light" && <HiOutlineMoon></HiOutlineMoon>}
+            <AnimatePresence>
+              {props.theme === "light" && (
+                <motion.span
+                  layout
+                  animate={{ scale: 1 }}
+                  initial={{ scale: 0 }}
+             
+                >
+                  <HiOutlineMoon className="w-5 h-5"></HiOutlineMoon>
+                </motion.span>
+              )}
+            </AnimatePresence>
             {props.theme === "light" && (
               <span className="pl-3 font-medium">Dark Mode</span>
             )}
-            {props.theme === "dark" && <MdOutlineLightMode></MdOutlineLightMode>}
+            <AnimatePresence>
+              {props.theme === "dark" && (
+                <motion.span
+                  layout
+                  animate={{ scale: 1 }}
+                  initial={{ scale: 0 }}
+                 
+                >
+                  <MdOutlineLightMode></MdOutlineLightMode>
+                </motion.span>
+              )}
+            </AnimatePresence>
             {props.theme === "dark" && (
               <span className="pl-3 font-medium">Light Mode</span>
             )}
