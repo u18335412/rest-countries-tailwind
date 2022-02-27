604cfa04cf274d3b91e93e772f87d392043a705c
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-02-27 18:14:19 +0200

    refactor

--- pages/index.js
+++ pages/index.js
@@ -83,7 +83,11 @@ const Home = ({ countries }) => {
       <Head>
         <title>Countries</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={true}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600&display=swap"
           rel="stylesheet"
